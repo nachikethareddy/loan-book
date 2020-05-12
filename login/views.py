5d548c469e93b91e6466334e4d7d35f5b467a23a
Nachiketha Reddy <c.nachikethareddy@gmail.com>
2020-05-12 19:04:46 +0530

    'finalllllly'

--- login/views.py
+++ login/views.py
@@ -14,5 +14,8 @@ def login(request):
 
     myobj = {'username': u,'password':p}
     x = requests.post(url, data = myobj)
-    data=json.loads(x.json)
-    return render(request,'a.html')
+    data=json.loads(x.text)
+    if ('auth_token' in data.keys()):
+        return render(request,'a.html',{'token':data['auth_token']})
+    else:
+        return render(request,'index.html',{'auth':'Invalid username/password'})
